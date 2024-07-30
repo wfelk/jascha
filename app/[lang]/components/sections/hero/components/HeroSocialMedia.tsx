820f535d415ca11e6a8a3820b97859dc9b0f5c79
Wilhelm Felk <wilhelm.felk@adesso.de>
2024-07-30 20:15:09 +0200

    Add threads icon to social media bar

--- app/[lang]/components/sections/hero/components/HeroSocialMedia.tsx
+++ app/[lang]/components/sections/hero/components/HeroSocialMedia.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faMeetup } from '@fortawesome/free-brands-svg-icons';
+import {
+  faInstagram,
+  faMeetup,
+  faThreads,
+} from '@fortawesome/free-brands-svg-icons';
 
 const socialMediaHandles = [
   { icon: faMeetup, url: 'https://www.meetup.com/jascha-cologne/' },
-  { icon: faInstagram, url: 'https://www.instagram.com/jascha.cologne/' },
+  { icon: faInstagram, url: 'https://www.instagram.com/jascha.club/' },
+  { icon: faThreads, url: 'https://www.threads.net/jascha.club' },
 ];
 
 const HeroSocialMedia: FC = () => {
