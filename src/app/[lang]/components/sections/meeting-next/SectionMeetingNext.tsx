c2d3b03ff4048fc121d749b76a88022237c4a4be
wfelk <w.felk@me.com>
2024-10-21 22:24:25 +0200

    Display next meeting dynamically

--- src/app/[lang]/components/sections/meeting-next/SectionMeetingNext.tsx
+++ src/app/[lang]/components/sections/meeting-next/SectionMeetingNext.tsx
@@ -9,10 +9,10 @@ interface Props {
   infoOnMeetings: InfoOnMeetings;
 }
 
-const SectionMeetingNext: FC<Props> = ({ infoOnMeetings }) => {
+const SectionMeetingNext: FC<Props> = ({
+  infoOnMeetings: { isNextMeetingScheduled = false },
+}) => {
   const t = useTranslations('sections.nextMeeting');
-  const isNextMeetingScheduled =
-    infoOnMeetings?.isNextMeetingScheduled ?? false;
   return (
     <section
       className="md:col-span-8 md:max-h-[80vh] md:col-start-3 lg:max-h-full lg:col-start-2 lg:col-span-4 mt-32 md:m-0"
