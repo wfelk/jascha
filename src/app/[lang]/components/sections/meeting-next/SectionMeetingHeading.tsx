0db12d361c3f4b98c1eb4e55852baa47f54e1861
wfelk <w.felk@me.com>
2024-10-21 22:24:35 +0200

    Increase width of heading

--- src/app/[lang]/components/sections/meeting-next/SectionMeetingHeading.tsx
+++ src/app/[lang]/components/sections/meeting-next/SectionMeetingHeading.tsx
@@ -8,7 +8,7 @@ interface Props {
 const SectionHeading: FC<Props> = ({ label, isNextMeetingScheduled }) => {
   return (
     <h2
-      className={`sm:text-xl xl:text-3xl  h-1/8 tracking-wide font-light text-2xl p-4 rounded-l-lg px-6w-1/2 self-end md:self-center md:px-8 md:rounded-lg md:w-full md:text-center md:tracking-wider ${isNextMeetingScheduled ? 'md:bg-teal-700/40 bg-teal-600/50 text-teal-200/75' : ' md:bg-orange-700/40 bg-orange-600/50 text-orange-200/75 '}`}
+      className={`sm:text-xl xl:text-3xl  h-1/8 tracking-wide font-light text-2xl p-4 rounded-l-lg px-6w-1/2 self-end md:self-center md:px-8 md:rounded-lg w-1/2 md:w-full md:text-center md:tracking-wider ${isNextMeetingScheduled ? 'md:bg-teal-700/40 bg-teal-600/50 text-teal-200/75' : ' md:bg-orange-700/40 bg-orange-600/50 text-orange-200/75'}`}
     >
       {label}
     </h2>
