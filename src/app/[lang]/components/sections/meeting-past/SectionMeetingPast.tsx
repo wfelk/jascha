8d61e100e753aacbaec71c4d7e13bfd3922b57b4
wfelk <w.felk@me.com>
2024-10-22 22:13:32 +0200

    Fix swiper preventing clicks in the vicinity

--- src/app/[lang]/components/sections/meeting-past/SectionMeetingPast.tsx
+++ src/app/[lang]/components/sections/meeting-past/SectionMeetingPast.tsx
@@ -9,7 +9,7 @@ import { useTranslations } from 'next-intl';
 const SectionMeetingPast: FC = () => {
   const t = useTranslations('sections.photos');
   return (
-    <section className="flex flex-col max-h-full will-change-scroll mt-32 md:mt-0 gap-12 md:col-span-8 md:col-start-3 xl:col-start-3 xl:col-span-8">
+    <section className="flex flex-col max-h-full will-change-scroll mt-32 md:mt-0 gap-12 md:col-span-8 md:col-start-3 xl:col-start-3 xl:col-span-8 overflow-y-clip">
       <SectionHeading label={t('heading')} />
       <MeetingPastMobile />
       <MeetingPastDesktop />
