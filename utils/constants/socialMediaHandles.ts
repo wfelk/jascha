9c2021f7091cf559c9939874ce996838074af4cb
Wilhelm Felk <w.felk@me.com>
2024-11-02 00:25:53 +0100

    Remove WhatsApp from social media handles

--- utils/constants/socialMediaHandles.ts
+++ utils/constants/socialMediaHandles.ts
@@ -1,7 +1,7 @@
 import { faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 const SOCIAL_MEDIA_HANDLES = [
-  { icon: faWhatsapp, url: 'https://chat.whatsapp.com/DGBQ1s8ZdsKItnTvZRxEQj' },
+  ,
   { icon: faInstagram, url: 'https://www.instagram.com/jascha.club/' },
 ];
 
