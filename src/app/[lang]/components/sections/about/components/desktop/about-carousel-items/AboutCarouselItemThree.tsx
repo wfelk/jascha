6cb3b38dd289f296ee1e9184d5442e562fc55617
Wilhelm Felk <w.felk@me.com>
2024-11-11 11:16:17 +0100

    Decrease line height

--- src/app/[lang]/components/sections/about/components/desktop/about-carousel-items/AboutCarouselItemThree.tsx
+++ src/app/[lang]/components/sections/about/components/desktop/about-carousel-items/AboutCarouselItemThree.tsx
@@ -7,7 +7,7 @@ const AboutCarouselItemThree = ({ className }: OnlyClassNameProps) => {
   return (
     <div className={`text-xl xl:text-2xl ${className}`}>
       <p className="font-light text-3xl xl:text-4xl">{t('paragraph1')}</p>
-      <p className="leading-loose">
+      <p>
         {t('paragraph2.beforeEmphasis')}{' '}
         <span className="font-bold text-sky-200">
           {t('paragraph2.emphasis')}
